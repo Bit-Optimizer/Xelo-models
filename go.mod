56ba1db776bccc0c80f4a67c81444b6a3ba06130
AyushRusia <ayushrusia11@outlook.com>
2022-04-04 23:15:48 +0530

    fix :Uppercase

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/Bit-Optimizer/Xelo-models
 
 go 1.17
 
-require go.mongodb.org/mongo-driver v1.8.4 // indirect
+require go.mongodb.org/mongo-driver v1.8.4
--- user/user.go
+++ user/user.go
@@ -3,24 +3,24 @@ package user
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Address struct {
-	country  string `bson:"country"`
-	division string `bson:"division"`
-	city     string `bson:"city"`
-	lane     string `bson:"lane"`
-	pin      string `bson:"pin"`
+	Country  string `bson:"country"`
+	Division string `bson:"division"`
+	City     string `bson:"city"`
+	Lane     string `bson:"lane"`
+	Pin      string `bson:"pin"`
 }
 
 type User struct {
-	id        string   `bson:"_id,omitempty"`
-	name      string   `bson:"name"`
-	email     string   `bson:"email"`
-	phone     string   `bson:"phone"`
-	profile   string   `bson:"profile"`
-	address   Address  `bson:"address"`
-	rating    float32  `bson:"rating"`
-	ratecount int32    `bson:"ratecount"`
-	wishlist  []string `bson:"wishlist"`
-	products  []string `bson:"products"`
-	createdAt primitive.DateTime `bson:"createdAt"`
-	updatedAt primitive.DateTime `bson:"updatedAt"`
+	Id        string   `bson:"_id,omitempty"`
+	Name      string   `bson:"name"`
+	Email     string   `bson:"email"`
+	Phone     string   `bson:"phone"`
+	Profile   string   `bson:"profile"`
+	Address   Address  `bson:"address"`
+	Rating    float32  `bson:"rating"`
+	Ratecount int32    `bson:"ratecount"`
+	Wishlist  []string `bson:"wishlist"`
+	Products  []string `bson:"products"`
+	CreatedAt primitive.DateTime `bson:"createdAt"`
+	UpdatedAt primitive.DateTime `bson:"updatedAt"`
 }
